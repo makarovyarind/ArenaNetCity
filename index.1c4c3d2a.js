295364abb17f750b8fa1bbf0ce7cda22bb337933
makarovyarind <110284582+makarovyarind@users.noreply.github.com>
2023-12-03 19:22:52 +0000

    Deploying to gh-pages from @ makarovyarind/ArenaNetCity@b6ec29d7036c09645976d0769c4aec5902960905 🚀

---
+++ index.1c4c3d2a.js
@@ -0,0 +1,2 @@
+const e=new GraphTabs("tab",{isChanged:e=>{console.log(e)}}),a=new GraphTabs("tab3",{isChanged:e=>{console.log(e)}});e.switchTabs(document.querySelector("#tab")),a.switchTabs(document.querySelector("#tab3"));
+//# sourceMappingURL=index.1c4c3d2a.js.map
---
+++ index.a38e55e5.js
@@ -0,0 +1,2 @@
+!function(){var e=new GraphTabs("tab",{isChanged:function(e){console.log(e)}}),n=new GraphTabs("tab3",{isChanged:function(e){console.log(e)}});e.switchTabs(document.querySelector("#tab")),n.switchTabs(document.querySelector("#tab3"))}();
+//# sourceMappingURL=index.a38e55e5.js.map
